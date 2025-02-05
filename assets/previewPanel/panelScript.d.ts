f95dbcd517cef5221adf189cbe5a479888c48051
Abdelrahman <are@luftborn.com>
2025-02-05 12:49:27 +0200

    smol gitignore fixes

--- assets/previewPanel/panelScript.d.ts
+++
@@ -1,8 +0,0 @@
-interface vscode {
-    postMessage(message: any): void;
-}
-declare function acquireVsCodeApi(): vscode;
-declare const vscode: vscode;
-declare const mainDiv: HTMLElement;
-declare function replaceMainContent(html: string): void;
-declare function listenToVscodeMessages(): void;
